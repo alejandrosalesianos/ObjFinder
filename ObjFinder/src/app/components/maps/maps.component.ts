c67f28844ef71d733745b477ccaeee37d38d7288
alejandrosalesianos <alejandromartin2013@live.com>
2021-12-21 12:46:14 +0100

    Añadido funcion de localizar por el nombre de la calle

--- ObjFinder/src/app/components/maps/maps.component.ts
+++ ObjFinder/src/app/components/maps/maps.component.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Timestamp } from 'firebase/firestore';
-import { catchError, map, of } from 'rxjs';
+import { catchError, last, map, of } from 'rxjs';
 import { Observable, } from 'rxjs';
 import { Categorias } from 'src/app/interface/categorias.interface';
 import Lista from 'src/app/interface/lista.interface';
@@ -22,6 +22,7 @@ export class MapsComponent implements OnInit {
   apiLoaded: Observable<boolean>;
   address: string = '';
   fecha!: Timestamp;
+  location!: google.maps.LatLngLiteral;
 
   id!: string;
   descripcion: string[] = [];
@@ -44,8 +45,17 @@ export class MapsComponent implements OnInit {
   }
 
   searchAddress(){
-    let addressSplited = this.address.split(',');
-    this.fundacionDonBoscoLatLng = {lat:Number(addressSplited[0]),lng: Number(addressSplited[1])}
+    //let addressSplited = this.address.split(',');
+    //this.fundacionDonBoscoLatLng = {lat:Number(addressSplited[0]),lng: Number(addressSplited[1])}
+    let geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { 'address' : this.address}, function(results,status){
+      if(status == google.maps.GeocoderStatus.OK){
+        let localizacion = (results[0])
+        localStorage.setItem('lng',localizacion.geometry.location.lng().toString())
+        localStorage.setItem('lat',localizacion.geometry.location.lat().toString())
+      }
+    });
+    console.log(geocoder)
     console.log(this.address)
   }
   updateLocationMarker(event: google.maps.MapMouseEvent){
@@ -73,13 +83,15 @@ export class MapsComponent implements OnInit {
 
   crearObjeto(){
     let id = localStorage.getItem('uid')
-    let address = localStorage.getItem('location');
-    address?.split(' ')
+    let lat = Number(localStorage.getItem('lat'));
+    let lng = Number(localStorage.getItem('lng'))
+    this.location = {lat: lat, lng: lng}
+    console.log(this.location)
   this.firestore.collection(`users/${id}/${this.tipo}`).add({
     name: this.objeto,
     categoria: this.id,
     descripcion: this.descripcion,
-    location: address,
+    location: this.location,
     fecha: this.fecha});
 }
 }
